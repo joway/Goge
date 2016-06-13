ae15ac61ef632677dbc7e42904949fa305f41c9b
joway <joway.w@gmail.com>
2016-06-13 16:54:43 +0800

    debug

--- config/settings.py
+++ config/settings.py
@@ -245,9 +245,10 @@ if REDIS_PASS is None:
 else:
     REDIS_URL = 'redis://:' + REDIS_PASS + '@' + REDIS_HOST + ':' + REDIS_PORT
 
-BROKER_URL = REDIS_URL
-CELERY_RESULT_BACKEND = REDIS_URL
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Shanghai'
+
+BROKER_URL = 'redis://localhost:6379/'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TIMEZONE = 'Asia/Shanghai'
